c86d64c3452cbbd570b1969cfda33c9b046a3674
Michael Ingleton <mingleton42@gmail.com>
2019-06-06 12:37:59 -0600

    built out layout grid and headernav components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
 
 class Layout extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Ecom</h1>
-        {this.props.children}
+      <div className='layout'>
+        {/* <h1>Ecom</h1>
+        {this.props.children} */}
+        <Header/>
+        <Navbar />
       </div>
     );
   }
