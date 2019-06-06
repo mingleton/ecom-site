a4b98978291156d48606f92d1a5c7679873b69f4
Michael Ingleton <mingleton42@gmail.com>
2019-06-06 13:02:31 -0600

    built signin and signup routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    CanvasRenderingContext2D() {
+        return (
+            <div className = 'sign-in'>
+                sign in
+            </div>
+        )
+    }
+}
+
+export default SignIn
---
+++ src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    CanvasRenderingContext2D() {
+        return (
+            <div className = 'sign-up'>
+            </div>
+        )
+    }
+}
+
+export default SignUp
