40710543f9adab2c7771e27dff98b21b6dd670eb
Michael Ingleton <mingleton42@gmail.com>
2019-06-06 14:12:07 -0600

    created page title component and imported custom fonts

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
-
+import PageTitle from '../pageTitle';
 
 class SignIn extends Component {
 
@@ -12,6 +12,8 @@ class SignIn extends Component {
     render() {
         return (
             <div className = 'sign-in'>
+                <PageTitle className= 'sign-in__page-title'
+                title='Login' />
                 <SignInForm 
                 onSubmit={this.onSubmit}
                 className= 'sign-in__form'/>
