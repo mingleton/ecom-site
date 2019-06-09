b9da0419e8425ed6a4a9766ede08faab7487f9ee
Michael Ingleton <mingleton42@gmail.com>
2019-06-09 16:00:55 -0600

    developed sign up form and edited gray back button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
+import SignUpForm from './signupForm';
+import PageTitle from '../pageTitle';
+
 class SignUp extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields)
+    }
+
     render() {
         return (
             <div className = 'sign-up'>
+                <PageTitle className='sign-up__page-title'
+                title='Register' />
+                <SignUpForm 
+                    className= 'sign-up__form'
+                    onSubmit={this.onSubmit}
+                />
             </div>
         )
     }
 }
+
 
 export default SignUp
