8d249125f9e3197508c2cf04f9ad51411110f5c1
Michael Ingleton <mingleton42@gmail.com>
2019-06-12 17:11:06 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,71 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase1',
-                amount: 8.02
+                total: 19.69,
+                orderNumber: 'A00069696',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Michael Ingleton',
+                    shippingAddress: '2134 west state',
+                }
             },
             {
                 _id: 1,
-                title: 'purchase2',
-                amount: 19.69
-            }
+                total: 19.69,
+                orderNumber: '1738ldkjle',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Kawhi Leonard',
+                    shippingAddress: '3737 west state',
+                }
+            },
+            {
+                _id: 2,
+                total: 19.69,
+                orderNumber: 'A00099696',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Greg Ingleton',
+                    shippingAddress: '2134 east state',
+                }
+            },
+            {
+                _id: 3,
+                total: 19.69,
+                orderNumber: 'A00069696',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Michael Ingleton',
+                    shippingAddress: '2134 west state',
+                }
+            },
+            {
+                _id: 4,
+                total: 19.60,
+                orderNumber: 'AAAAA000043',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Michael Jordan',
+                    shippingAddress: '2134 south state',
+                }
+            },
+            {
+                _id: 5,
+                total: 19.69,
+                orderNumber: 'KELA49333',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Michael Phelps',
+                    shippingAddress: '2134 west oak',
+                }
+            },
+            
         ]
     })
 }
