f31359768f2abc56464123bf1e0679c89cecf53e
Michael Ingleton <mingleton42@gmail.com>
2019-06-12 17:22:18 -0600

    implemented set purchase detail action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,11 +5,13 @@ import {
 } from './headernavbar';
 
 import {
-    fetchUserPurchases
+    fetchUserPurchases,
+    setPurchaseDetail
 } from './user';
 export {
     setHeaderLinks,
     setNavbarLinks,
     changeNavbarActive,
-    fetchUserPurchases
+    fetchUserPurchases,
+    setPurchaseDetail
 };
