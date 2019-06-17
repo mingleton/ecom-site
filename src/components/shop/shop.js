c74570b60a1650d887f4f4dd3ee85a3a359de9b6
Michael Ingleton <mingleton42@gmail.com>
2019-06-17 11:52:03 -0600

    styled shop grids finished back of shop product component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -39,7 +39,7 @@ class Shop extends Component {
     render() {
         return (
         <div className='shop'>
-            <ShopSearchBar onSubmit={this.onSubmit} className='shop-search-bar__search-bar' />
+            <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar' />
             <div className = 'shop__products'>
               {
                   this.props.filteredProducts.map(product => {
