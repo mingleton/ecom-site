1ec391d6d2cba8ee482c96afeef6bd4edcbf57fe
Michael Ingleton <mingleton42@gmail.com>
2019-06-17 16:07:47 -0600

    finished cart styles

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -38,7 +38,7 @@ class Shop extends Component {
     }
 
     render() {
-        return <ShopCart className='shop__cart' />
+        // return <ShopCart className='shop__cart' />
 
         return (
         <div className='shop'>
