d412d6c2e7f560ff3da83f6babb9784c35cb8ad5
Michael Ingleton <mingleton42@gmail.com>
2019-06-18 16:30:07 -0600

    build review product component

--- src/actions/user.js
+++ src/actions/user.js
@@ -30,7 +30,8 @@ export function fetchCartProducts() {
                 title: 'JavaScript in the Browser',
                 description: 'Cool sticker that shows stuff',
                 price: '1.99',
-                belongsTo: [0 ,1]
+                belongsTo: [0 ,1],
+                imageUrl: 'http://via.placeholder.com/80x80'
                 },
                 quantity: 2
             },
@@ -41,7 +42,8 @@ export function fetchCartProducts() {
                 title: 'Graph Database',
                 description: 'Cool sticker that shows stuff',
                 price: '1.99',
-                belongsTo: [0 ,6]
+                belongsTo: [0 ,6],
+                imageUrl: 'http://via.placeholder.com/80x80'
             },
             quantity: 1
         }
--- src/components/order/reviewProducts.js
+++ src/components/order/reviewProducts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
+import ProductReview from './productReview';
 
 
 class ReviewProducts extends Component {
@@ -9,7 +10,10 @@ class ReviewProducts extends Component {
             <div className= {`${className} review-products`}>
                 {
                     this.props.cartProducts.map(cartProduct => {
-                        return <h1 key={cartProduct._id}>{cartProduct.product.title}</h1>
+                        // return <h1 key={cartProduct._id}>{cartProduct.product.title}</h1>
+                       return <ProductReview key={cartProduct._id}
+                        {...cartProduct}
+                        />
                     })
                 }
             </div>
